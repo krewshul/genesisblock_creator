d16d81f5c1e4a3869eb1d76645a55a80b3ca9736
jeremy lee <31496594+krewshul@users.noreply.github.com>
2025-04-12 14:38:36 -0500

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -1,10 +1,11 @@
-import hashlib, binascii, struct, array, os, time, sys, argparse
+import hashlib, struct, os, time, sys, argparse
 import scrypt
 from construct import *
 
 
 def main():
     options = get_args()
+
     algorithm = get_algorithm(options)
 
     input_script = create_input_script(options.timestamp)
@@ -21,46 +22,39 @@ def main():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--time", dest="time", default=int(time.time()),
-                        type=int, help="the (unix) time when the genesisblock is created")
+    parser.add_argument("-t", "--time", dest="time", default=int(time.time()), type=int)
     parser.add_argument("-z", "--timestamp", dest="timestamp",
                         default="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks",
-                        type=str, help="the pszTimestamp found in the coinbase of the genesisblock")
-    parser.add_argument("-n", "--nonce", dest="nonce", default=0,
-                        type=int, help="the first value of the nonce that will be incremented when searching the genesis hash")
-    parser.add_argument("-a", "--algorithm", dest="algorithm", default="SHA256",
-                        help="the PoW algorithm: [SHA256|scrypt|X11|X13|X15]")
+                        type=str)
+    parser.add_argument("-n", "--nonce", dest="nonce", default=0, type=int)
+    parser.add_argument("-a", "--algorithm", dest="algorithm", default="SHA256")
     parser.add_argument("-p", "--pubkey", dest="pubkey",
                         default="04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f",
-                        type=str, help="the pubkey found in the output script")
-    parser.add_argument("-v", "--value", dest="value", default=5000000000,
-                        type=int, help="the value in coins for the output")
-    parser.add_argument("-b", "--bits", dest="bits", type=int,
-                        help="the target in compact representation")
+                        type=str)
+    parser.add_argument("-v", "--value", dest="value", default=5000000000, type=int)
+    parser.add_argument("-b", "--bits", dest="bits", type=int)
 
     options = parser.parse_args()
 
     if not options.bits:
-        if options.algorithm in ["scrypt", "X11", "X13", "X15"]:
-            options.bits = 0x1e0ffff0
-        else:
-            options.bits = 0x1d00ffff
+        options.bits = 0x1e0ffff0 if options.algorithm in ["scrypt", "X11", "X13", "X15"] else 0x1d00ffff
 
     return options
 
 
 def get_algorithm(options):
     supported_algorithms = ["SHA256", "scrypt", "X11", "X13", "X15"]
-    if options.algorithm in supported_algorithms:
-        return options.algorithm
-    else:
+    if options.algorithm not in supported_algorithms:
         sys.exit("Error: Given algorithm must be one of: " + str(supported_algorithms))
+    return options.algorithm
 
 
 def create_input_script(psz_timestamp):
     psz_prefix = "4c" if len(psz_timestamp) > 76 else ""
     script_prefix = '04ffff001d0104' + psz_prefix + format(len(psz_timestamp), '02x')
-    return bytes.fromhex(script_prefix + psz_timestamp.encode().hex())
+    full_script = script_prefix + psz_timestamp.encode().hex()
+    print(full_script)
+    return bytes.fromhex(full_script)
 
 
 def create_output_script(pubkey):
@@ -123,13 +117,13 @@ def generate_hash(data_block, algorithm, start_nonce, bits):
     print("Searching for genesis hash...")
     nonce = start_nonce
     last_updated = time.time()
-    target = (bits & 0xffffff) * 2**(8 * ((bits >> 24) - 3))
+    target = (bits & 0xffffff) * 2 ** (8 * ((bits >> 24) - 3))
 
     while True:
         sha256_hash, header_hash = generate_hashes_from_block(data_block, algorithm)
         last_updated = calculate_hashrate(nonce, last_updated)
         if is_genesis_hash(header_hash, target):
-            return (sha256_hash if algorithm != "X11" and algorithm != "X13" and algorithm != "X15" else header_hash, nonce)
+            return (sha256_hash if algorithm in ["SHA256", "scrypt"] else header_hash, nonce)
         nonce += 1
         data_block = data_block[:-4] + struct.pack('<I', nonce)
 
@@ -162,11 +156,10 @@ def calculate_hashrate(nonce, last_updated):
         now = time.time()
         hashrate = round(1000000 / (now - last_updated))
         generation_time = round(pow(2, 32) / hashrate / 3600, 1)
-        sys.stdout.write("\r{} hash/s, estimate: {} h".format(hashrate, generation_time))
+        sys.stdout.write(f"\r{hashrate} hash/s, estimate: {generation_time} h")
         sys.stdout.flush()
         return now
-    else:
-        return last_updated
+    return last_updated
 
 
 def print_block_info(options, hash_merkle_root):
